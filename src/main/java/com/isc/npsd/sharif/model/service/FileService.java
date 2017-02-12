6c6ddb7d220e121b14a61e85b3f2d5dabccf6f9e
alijohnkeh <alijohnkeh@gmail.com>
2017-02-12 09:58:25 +0330

    transaction key

--- src/main/java/com/isc/npsd/sharif/model/service/FileService.java
+++ src/main/java/com/isc/npsd/sharif/model/service/FileService.java
@@ -75,7 +75,7 @@ public class FileService extends BaseServiceImpl<File, FileRepository> {
                 Pipeline p = RedisUtil.getPipeline();
                 transactions.forEach(transaction -> {
                     try {
-                        p.set(transaction.getMndtReqId(), new ObjectMapper().writeValueAsString(transaction));
+                        p.set(transaction.getCBIC() + "," + transaction.getDBIC(), new ObjectMapper().writeValueAsString(transaction));
                     } catch (JsonProcessingException e) {
                         e.printStackTrace();
                     }
